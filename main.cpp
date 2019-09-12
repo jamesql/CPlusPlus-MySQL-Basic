e7a51a706ba30a5e0e005af7358959fcafef9539
James <contactsmitteh@gmail.com>
2019-09-11 22:01:14 -0400

    Add Main File

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QCoreApplication>
+#include <QPushButton>
+#include <QLabel>
+#include <QCheckBox>
+#include <QLineEdit>
+#include "sha256.h"
+#include "jdbc/mysql_connection.h"
+#include <jdbc/cppconn/driver.h>
+#include <jdbc/cppconn/exception.h>
+#include <jdbc/cppconn/resultset.h>
+#include <jdbc/cppconn/statement.h>
+
+using namespace ::sql;
+
+Driver *driver;
+Connection *con;
+Statement *stmt;
+ResultSet *res;
+
+void connectDatabase() {
+    driver = get_driver_instance();
+    con = driver->connect("tcp://127.0.0.1:3306", "root", "password");
+}
+
+void disconnectDb() {
+    con->close();
+    delete con;
+}
+
+void setSchema(std::string db) {
+    con->setSchema(db);
+}
+
+ResultSet* query(std::string statement) {
+    ResultSet *newRes;
+    stmt = con->createStatement();
+    newRes = stmt->executeQuery(statement);
+    return newRes;
+}
+
+int main() {
+  connectDatabase();
+  setSchema("database");
+  ResultSet *r = query("SELECT * FROM database");
+  while(r->next()) {
+    cout << res->getString(1) << endl;
+  }
+  return 0;
+}
+
